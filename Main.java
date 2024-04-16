c54c97aff966200fbe56d92e33ad47b9e205cec8
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2024-04-16 10:48:11 -0400

    Added main class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Student James = new Student("James", "Smith", 11);
+        System.out.println(James.ToString());
+    }
+}
--- Student.java
+++ Student.java
@@ -2,4 +2,14 @@ public class Student {
     public String firstName;
     public String lastName;
     public int gradeLevel;
+
+    public Student(String first, String last, int grade) {
+        firstName = first;
+        lastName = last;
+        gradeLevel = grade;
+    }
+
+    public String ToString() {
+        return "Name: " + firstName + " " + lastName + ", Grade: " + gradeLevel;
+    }
 }
