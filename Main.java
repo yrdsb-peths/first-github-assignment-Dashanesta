928d5982a3f70852e548d453bea793cacf22df97
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2024-04-17 13:19:38 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,29 +4,33 @@ public class Main {
     String repeat = "yes";
 
         while(repeat.equalsIgnoreCase("yes")) {
-            String firstName = System.console().readLine("Enter a student's first name: ");
-            String lastName = System.console().readLine("Enter the student's last name: ");
-            int gradeLevel;
             
-            while(true) {
-                try {
-                    gradeLevel = Integer.valueOf(System.console().readLine("Enter the student's grade level: "));
-                    break;
-                } catch(Exception e) {
-                    System.out.print("Invalid grade level. ");
-                }
-            }
-
-            Student student = new Student(firstName, lastName, gradeLevel);
-            System.out.println(student.toString());
+            NewStudent();
 
             repeat = System.console().readLine("Would you like to continue adding students? ");
-
             while(!(repeat.equalsIgnoreCase("yes") || repeat.equalsIgnoreCase("no"))) {
                 repeat = System.console().readLine("Invalid input, please try again: ");
             }
         }
 
         System.out.println("Total Students: " + Student.totalStudents);
+    }
+
+    public static void NewStudent() {
+        String firstName = System.console().readLine("Enter a student's first name: ");
+        String lastName = System.console().readLine("Enter the student's last name: ");
+        int gradeLevel;
+        
+        while(true) {
+            try {
+                gradeLevel = Integer.valueOf(System.console().readLine("Enter the student's grade level: "));
+                break;
+            } catch(Exception e) {
+                System.out.print("Invalid grade level. ");
+            }
+        }
+
+        Student student = new Student(firstName, lastName, gradeLevel);
+        System.out.println(student.toString());
     }
 }
